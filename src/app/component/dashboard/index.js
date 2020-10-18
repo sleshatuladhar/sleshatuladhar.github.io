9fb8d9f0e2000b268ea46d59eceab84ebccaeeaa
slesha <slesha@noveltytechnology.com>
2020-10-18 19:49:56 +0545

    min-height changed to minHeight

--- src/app/component/dashboard/index.js
+++ src/app/component/dashboard/index.js
@@ -4,8 +4,8 @@
 
 import { MDBBtn, MDBCol } from 'mdbreact';
 import React, { Component } from 'react'
-import paroolYogaGroup from '../../../assets/img/png/parool-yoga-group.png';
 import commonConstants from '../../constant/commonConstants';
+import AboutUs from './aboutUs';
 
 class Dashboard extends Component {
   state = {
@@ -16,21 +16,7 @@ class Dashboard extends Component {
     return (
       <div className='container-fluid'>
         {/* About Us */}
-        <div className='about-us'>
-          <div className='d-flex justify-content-center align-items-center mt-5 mb-3'>
-            <h1 className='text-primary'>About Us</h1>
-          </div>
-          <div className='grey lighten-2 d-flex justify content-between mx-5 px-5 py-4'>
-            <img
-              src={paroolYogaGroup}
-              className='img-fluid cursor-pointer'
-              alt='parool-yoga-group'
-            />
-            <p className='text-justify ml-5'>
-              Maitreya Bodhi Home Care and Service has been opened with the vision to provide the best Home Care Service and Clinic facilities to the elderly. After years of dreaming and planning with ideas, Dr. Sampurna Man Tuladhar took the initiative to convert a derelict garage with a tin roof and mudbrick walls to a spacious and geriatic friendly clinic. The main idea behind Maitreya Bodhi Home Care and Clinic was to send elderly parents to the clinic to spend some quality time while their family goes to work. However, we are still encouraging elders to spend their time in the clinic.
-            </p>
-          </div>
-        </div>
+        <AboutUs />
         {/* End About Us */}
 
         {/* Range of our Healthcare Programs */}
@@ -46,7 +32,7 @@ class Dashboard extends Component {
                   <MDBCol md='3'>
                     <div
                       className='grey m-2 p-3 d-flex flex-column justify-content-between align-items-center align-self-stretch'
-                      style={{ 'min-height': '200px' }}
+                      style={{ minHeight: '200px' }}
                     >
                       <img
                         src={service.img}
