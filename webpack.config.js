5737c58c2d8a263bf33e4ac68d684be61bbf7975
slesha <slesha@noveltytechnology.com>
2020-10-19 18:50:16 +0545

    ejecting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = {
   // root: path.resolve(__dirname, '../', '../'),
   output: path.resolve(__dirname, '../', '../', 'build'),
-  entry: path.resolve(__dirname, '../', '../', './src/index.js'),
+  entry: path.resolve(__dirname, '../', '../', 'src/index.js'),
   // template: path.resolve(__dirname, '../', '../', 'public/index.html'),
 };
