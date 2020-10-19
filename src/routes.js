c723e20e6d01f97294d9bc23c991aeaf2eabf2f3
slesha <slesha@noveltytechnology.com>
2020-10-19 16:36:54 +0545

    HOTFIX: removing duplicate hasrouter

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react'
-import { HashRouter, HashRouter, Switch } from 'react-router-dom'
+import { HashRouter, Switch } from 'react-router-dom'
 import Dashboard from './app/component/dashboard';
 import MainLayout from './app/component/layout/main-layout';
 import PublicLayout from './app/component/layout/public-layout';
