cd919b9350f79997368ce9f165469c2eca37ac0f
slesha <slesha@noveltytechnology.com>
2020-10-19 16:35:16 +0545

    browser router  changed to hashrouter

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@
  */
 
 import React from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { HashRouter, HashRouter, Switch } from 'react-router-dom'
 import Dashboard from './app/component/dashboard';
 import MainLayout from './app/component/layout/main-layout';
 import PublicLayout from './app/component/layout/public-layout';
 import AppRoute from './appRoute';
 
 const routes = (
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <AppRoute exact path="/" layout={MainLayout} component={Dashboard} />
       <AppRoute path="/dashboard" layout={MainLayout} component={Dashboard} />
@@ -18,7 +18,7 @@ const routes = (
       {/* redirect to default page if no route found */}
       <AppRoute path='*' layout={PublicLayout} component={Dashboard} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default routes;
