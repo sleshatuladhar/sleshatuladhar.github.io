82b6aeffb78e27e98d3e3ac1c5625cf87c3e63be
slesha <slesha@noveltytechnology.com>
2020-10-19 20:20:07 +0545

    deployment changes

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  // root: path.resolve(__dirname, '../', '../'),
-  output: path.resolve(__dirname, '../', '../', 'build'),
-  entry: path.resolve(__dirname, '../', '../', 'src/index.js'),
-  // template: path.resolve(__dirname, '../', '../', 'public/index.html'),
-};
